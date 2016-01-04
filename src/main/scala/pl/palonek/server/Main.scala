5245e2af69cf609f89bf237e36638a363374ef41
Lukasz Palonek <lpalonek@cyfronet.pl>
2016-01-04 09:02:45 +0100

    Added files route

--- src/main/scala/pl/palonek/server/Main.scala
+++ src/main/scala/pl/palonek/server/Main.scala
@@ -1,8 +1,24 @@
 package pl.palonek.server
 
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+
+
 /**
   * Created by lpalonek on 02/01/16.
   */
 object Main {
 
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+
+  def main(args: Array[String]) {
+    val serverRoute = new ServerRoute
+    val route = serverRoute.route
+    val config = ConfigFactory.load()
+    val bindingFuture = Http().bindAndHandle(route, config.getString("http.address"), config.getInt("http.port"))
+  }
 }
